a40fa778d8797e63fd847a00f9f534855ed12d7d
Jaydeep Machhi <jaydeepmachhi999997@gmail.com>
2020-08-14 01:08:53 +0530

    Add files via upload

---
+++ Binary Power.cpp
@@ -0,0 +1,37 @@
+// (1) With Modulo
+
+long long power_mod(long long a,long long b,long long m=mod)
+{
+    long long res=1;
+    a%=m;
+    while(b>0)
+    {
+        if(b&1)
+        {
+            res=(res*a)%m;
+        }
+        b>>=1;
+        a=(a*a)%m;
+    }
+    return res;
+}
+
+
+// (2) Without Modulo
+
+template <typename T>
+
+T power(T a,long long b)
+{
+    T res=1;
+    while(b>0)
+    {
+        if(b&1)
+        {
+            res=(res*a);
+        }
+        b>>=1;
+        a=(a*a);
+    }
+    return res;
+}
