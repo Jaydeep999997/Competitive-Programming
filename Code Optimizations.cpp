4e5535a3588a3088dd9067b70a80e2dda3893dc0
Jaydeep Machhi <jaydeepmachhi999997@gmail.com>
2021-05-12 15:52:43 +0530

    Create Code Optimizations.cpp

---
+++ Code Optimizations.cpp
@@ -0,0 +1,4 @@
+// Problem0: https://codeforces.com/contest/1511/problem/G
+// Solution0: https://codeforces.com/contest/1511/submission/113030323
+// Solution0: https://codeforces.com/contest/1511/submission/116012604
+// Solution0: https://codeforces.com/contest/1511/submission/116012773
