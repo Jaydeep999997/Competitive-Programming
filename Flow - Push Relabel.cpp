b770b360d1b3e773121ed82187ac3e620aedf72c
Jaydeep Machhi <jaydeepmachhi999997@gmail.com>
2021-05-19 18:58:40 +0530

    Create Flow - Push Relabel.cpp

---
+++ Flow - Push Relabel.cpp
@@ -0,0 +1,78 @@
+// Reference: https://cp-algorithms.com/graph/push-relabel.html
+// Time Complexity: O(V^2 * E)
+
+#define int long long   // Overflow!
+const int inf = 1e18;
+
+int n, m;
+vector<vector<int>> capacity, flow;
+vector<int> height, excess, seen;
+queue<int> excess_vertices;
+
+void push(int u, int v)
+{
+	int d = min(excess[u], capacity[u][v] - flow[u][v]);
+	flow[u][v] += d;
+	flow[v][u] -= d;
+	excess[u] -= d;
+	excess[v] += d;
+	if (d && excess[v] == d)
+		excess_vertices.push(v);
+}
+
+void relabel(int u)
+{
+	int d = inf;
+	for (int i = 0; i < n; i++) {
+		if (capacity[u][i] - flow[u][i] > 0)
+			d = min(d, height[i]);
+	}
+	if (d < inf)
+		height[u] = d + 1;
+}
+
+void discharge(int u)
+{
+	while (excess[u] > 0) {
+		if (seen[u] < n) {
+			int v = seen[u];
+			if (capacity[u][v] - flow[u][v] > 0 && height[u] > height[v])
+				push(u, v);
+			else
+				seen[u]++;
+		} else {
+			relabel(u);
+			seen[u] = 0;
+		}
+	}
+}
+
+int max_flow(int s, int t)
+{
+	height.assign(n, 0);
+	height[s] = n;
+	flow.assign(n, vector<int>(n, 0));
+	excess.assign(n, 0);
+	excess[s] = inf;
+	for (int i = 0; i < n; i++) {
+		if (i != s)
+			push(s, i);
+	}
+	seen.assign(n, 0);
+
+	while (!excess_vertices.empty()) {
+		int u = excess_vertices.front();
+		excess_vertices.pop();
+		if (u != s && u != t)
+			discharge(u);
+	}
+
+	int max_flow = 0;
+	for (int i = 0; i < n; i++)
+		max_flow += flow[i][t];
+	return max_flow;
+}
+
+
+// Problem0: https://cses.fi/problemset/task/1694/
+// Solution0: https://cses.fi/paste/bb5c0d9ec3ee712b213467/,  https://pastebin.com/XQLuZfVM
