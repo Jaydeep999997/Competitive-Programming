1b8a085c6c6061f7a72930b03f7302665c316253
Jaydeep Machhi <jaydeepmachhi999997@gmail.com>
2021-08-24 16:48:57 +0530

    Default Value added

--- Segment Tree Iterative.cpp
+++ Segment Tree Iterative.cpp
@@ -2,37 +2,44 @@ template<typename T>
 class segment	// pass argument as (L,R+1) to process the query from L to R
 {
 public:
+
 	vector<T> st;
-	ll n;
+	T defVal;
+	int n;
+
 	segment() {}
-	segment(ll _n)
+
+	segment(int _n, T _defVal = T())
 	{
 		n = _n;
-		st.resize(2 * n);
+		defVal = _defVal;
+		st.resize(2 * n, defVal);
 	}
+
 	T merge(T a, T b)
 	{
-		return max(a, b);
+		return (a + b);
 	}
+
 	void create()
 	{
-		for (ll i = n - 1; i > 0; i--)
+		for (int i = n - 1; i > 0; i--)
 		{
 			st[i] = merge(st[i << 1], st[(i << 1) | 1]);
 		}
 	}
-	void update(ll idx, T val)
+
+	void update(int idx, T val)
 	{
 		for (st[idx += n - 1] = val; idx > 1; idx >>= 1)
 		{
 			st[idx >> 1] = merge(st[min(idx, idx ^ 1)], st[max(idx, idx ^ 1)]);
 		}
 	}
-	T query(ll l, ll r)
+
+	T query(int l, int r)
 	{
-		T ans{};
-		T ansl{};
-		T ansr{};
+		T ans = defVal, ansl = defVal, ansr = defVal;
 		for (l += n - 1, r += n - 1; l < r; l >>= 1, r >>= 1)
 		{
 			if (l & 1)
