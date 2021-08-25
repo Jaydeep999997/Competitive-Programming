1d5dc6542f646ba22ea2532ee0ec24ff4de387c8
Jaydeep Machhi <jaydeepmachhi999997@gmail.com>
2021-08-25 21:17:55 +0530

    Template added

--- Policiy Based DS.cpp
+++ Policiy Based DS.cpp
@@ -4,7 +4,8 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // order_of_key(k) : returns number of elements strictly less than k
 // find_by_order(k) : returns kth smallest element from the set indexing from 0
@@ -15,7 +16,8 @@ using namespace __gnu_pbds;
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
-#define ordered_set tree<int, null_type,greater<int>, rb_tree_tag,tree_order_statistics_node_update>
+template<typename T>
+using ordered_set = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // order_of_key(k) : returns number of elements greater or equal to k
 // find_by_order(k) : returns kth biggest element from the set indexing from 0
