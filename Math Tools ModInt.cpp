8fd77f3712764bf19d360b8e3fec397aff2e2bc3
Jaydeep Machhi <jaydeepmachhi999997@gmail.com>
2021-08-26 01:31:31 +0530

    Update Math Tools ModInt.cpp

--- Math Tools ModInt.cpp
+++ Math Tools ModInt.cpp
@@ -2,53 +2,53 @@ template<const int &MOD>
 struct _m_int  // This is our new type which will do operations under modulo MOD
 {
 	int val;   // Value of variable
-
+ 
 	_m_int(int64_t v = 0)  // Typecasting into our data type from 64 bit integer
 	{
 		if (v < 0) v = v % MOD + MOD;
 		if (v >= MOD) v %= MOD;
 		val = v;
 	}
-
+ 
 	static int mod_inv(int a, int m = MOD)
 	{
 		// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Example
 		int g = m, r = a, x = 0, y = 1;
-
+ 
 		while (r != 0)
 		{
 			int q = g / r;
 			g %= r; swap(g, r);
 			x -= q * y; swap(x, y);
 		}
-
+ 
 		return x < 0 ? x + m : x;
 	}
-
+ 
 	explicit operator int() const
 	{
 		return val;
 	}
-
+ 
 	explicit operator int64_t() const
 	{
 		return val;
 	}
-
+ 
 	_m_int& operator += (const _m_int &other)  // Addition
 	{
 		val -= MOD - other.val;
 		if (val < 0) val += MOD;
 		return *this;
 	}
-
+ 
 	_m_int& operator -= (const _m_int &other)  // Subtraction
 	{
 		val -= other.val;
 		if (val < 0) val += MOD;
 		return *this;
 	}
-
+ 
 	static unsigned fast_mod(uint64_t x, unsigned m = MOD) // Mod operation
 	{
 #if !defined(_WIN32) || defined(_WIN64)
@@ -63,43 +63,43 @@ struct _m_int  // This is our new type which will do operations under modulo MOD
 		    : "d" (x_high), "a" (x_low), "r" (m));
 		return rem;
 	}
-
+ 
 	_m_int& operator*=(const _m_int &other)  // Multiplication
 	{
 		val = fast_mod((uint64_t) val * other.val);
 		return *this;
 	}
-
+ 
 	_m_int& operator/=(const _m_int &other)  // Division
 	{
 		return *this *= other.inv();
 	}
-
+ 
 	friend _m_int operator+(const _m_int &a, const _m_int &b) { return _m_int(a) += b; }
 	friend _m_int operator-(const _m_int &a, const _m_int &b) { return _m_int(a) -= b; }
 	friend _m_int operator*(const _m_int &a, const _m_int &b) { return _m_int(a) *= b; }
 	friend _m_int operator/(const _m_int &a, const _m_int &b) { return _m_int(a) /= b; }
-
+ 
 	_m_int& operator++()  // Pre-increment
 	{
 		val = val == MOD - 1 ? 0 : val + 1;
 		return *this;
 	}
-
+ 
 	_m_int& operator--()  // Pre-decrement
 	{
 		val = val == 0 ? MOD - 1 : val - 1;
 		return *this;
 	}
-
+ 
 	_m_int operator++(int) { _m_int before = *this; ++*this; return before; }  // Post increment
 	_m_int operator--(int) { _m_int before = *this; --*this; return before; }  // Post decrement
-
+ 
 	_m_int operator-() const  // Change sign
 	{
 		return val == 0 ? 0 : MOD - val;
 	}
-
+ 
 	// Boolean operators
 	bool operator==(const _m_int &other) const { return val == other.val; }
 	bool operator!=(const _m_int &other) const { return val != other.val; }
@@ -107,19 +107,19 @@ struct _m_int  // This is our new type which will do operations under modulo MOD
 	bool operator<=(const _m_int &other) const { return val <= other.val; }
 	bool operator> (const _m_int &other) const { return val >  other.val; }
 	bool operator>=(const _m_int &other) const { return val >= other.val; }
-
+ 
 	_m_int inv() const  // Calculating inverse
 	{
 		return mod_inv(val);
 	}
-
+ 
 	_m_int pow(int64_t p) const  // Calculating power
 	{
 		if (p < 0)
 			return inv().pow(-p);
-
+ 
 		_m_int a = *this, result = 1;
-
+ 
 		while (p > 0)
 		{
 			if (p & 1)
@@ -129,23 +129,23 @@ struct _m_int  // This is our new type which will do operations under modulo MOD
 			a *= a;
 			p >>= 1;
 		}
-
+ 
 		return result;
 	}
-
+ 
 	friend ostream& operator<<(ostream &os, const _m_int &m)  // Writing output
 	{
 		return os << m.val;
 	}
-
+ 
 };
-
-extern const int MOD = 998244353;
+ 
+extern const int MOD = 1e9 + 7;
 using mod_int = _m_int<MOD>;
-
+ 
 mod_int fact[N], ifact[N];
-
-
+ 
+ 
 void pre()
 {
 	fact[0] = 1;
@@ -160,10 +160,10 @@ void pre()
 		ifact[i] = (ifact[i + 1] * (i + 1));
 	}
 }
-
+ 
 mod_int ncr(int n, int r)
 {
-	if (r > n)
+	if (r > n || n < 0 || r < 0)
 	{
 		return 0;
 	}
@@ -171,10 +171,10 @@ mod_int ncr(int n, int r)
 	ans = (ans * fact[n]);
 	return ans;
 }
-
+ 
 mod_int incr(int n, int r)
 {
-	if (r > n)
+	if (r > n || n < 0 || r < 0)
 	{
 		return 0;
 	}
@@ -182,20 +182,20 @@ mod_int incr(int n, int r)
 	ans = (ans * ifact[n]);
 	return ans;
 }
-
+ 
 mod_int npr(int n, int r)
 {
-	if (r > n)
+	if (r > n || n < 0 || r < 0)
 	{
 		return 0;
 	}
 	mod_int ans = ifact[n - r] * fact[n];
 	return ans;
 }
-
+ 
 mod_int inpr(int n, int r)
 {
-	if (r > n)
+	if (r > n || n < 0 || r < 0)
 	{
 		return 0;
 	}
